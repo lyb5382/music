8bbfa889bce991ce4c8f80b30d2d0ef2251fd8ef
lyb5382 <lyb538202@gmail.com>
2025-05-23 19:53:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.querySelectorAll('.like-btn').forEach(btn => {
     btn.classList.toggle('liked');
   });
 });
+document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+document.getElementById('scrollTopBtn').addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.getElementById("darkToggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
